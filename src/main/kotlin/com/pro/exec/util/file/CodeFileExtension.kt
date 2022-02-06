5210022d0e26739a75a0bdfa60a9e904b13dbb1b
koreaaerospaceU <zx811@naver.com>
2022-02-06 19:48:02 +0900

    feat: 코드 파일 확장자 및 실행 파일 확장자 상수 정의

---
+++ src/main/kotlin/com/pro/exec/util/file/CodeFileExtension.kt
@@ -0,0 +1,10 @@
+package com.pro.exec.util.file
+
+/**
+ * Created by Minky on 2022-02-06
+ */
+object CodeFileExtension {
+    const val BASE: String = "codes"
+    const val C: String = ".c"
+    const val CPP: String = ".cpp"
+}
---
+++ src/main/kotlin/com/pro/exec/util/file/ExecutionFileExtension.kt
@@ -0,0 +1,10 @@
+package com.pro.exec.util.file
+
+/**
+ * Created by Minky on 2022-02-06
+ */
+object ExecutionFileExtension {
+    const val LINUX: String = ".su"
+    const val MAC: String = ".su"
+    const val WINDOW: String = ".exe"
+}
