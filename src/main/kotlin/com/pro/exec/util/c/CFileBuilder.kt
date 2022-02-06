663f05c6920e2cb9a00267f828a5da14a4ee7334
koreaaerospaceU <zx811@naver.com>
2022-02-06 19:46:57 +0900

    feat: C파일 컴파일 및 실행파일 생성 유틸 제작

---
+++ src/main/kotlin/com/pro/exec/util/c/CFileBuilder.kt
@@ -0,0 +1,54 @@
+package com.pro.exec.util.c
+
+import com.pro.exec.util.FileStatusSender
+import com.pro.exec.util.file.ExecutionFileExtension.WINDOW
+import java.io.File
+import java.util.concurrent.TimeUnit
+
+
+/**
+ * Created by Minky on 2022-02-06
+ */
+
+object CFileBuilder {
+    @Throws(Exception::class)
+    fun makeExecuteCode(fileStatusSender: FileStatusSender): FileStatusSender {
+        var status = true
+        val processBuilder = ProcessBuilder("gcc", fileStatusSender.fileName, "-o", fileStatusSender.originalFileName)
+        processBuilder.directory(File(fileStatusSender.directory))
+
+        val process = processBuilder.start()
+        process.waitFor(3, TimeUnit.SECONDS)
+        process.destroy()
+
+        val exitValue = process.exitValue()
+        if (exitValue != 0) {
+            status = false
+        }
+
+        val newFileName = "${fileStatusSender.originalFileName}${WINDOW}"
+        if (!checkFile("${fileStatusSender.directory}/$newFileName")) {
+            status = false
+        }
+
+        return FileStatusSender(
+            status,
+            fileStatusSender.originalFileName,
+            newFileName,
+            fileStatusSender.directory
+        )
+    }
+
+    private fun checkFile(path: String): Boolean {
+        val file = File(path)
+        return file.exists()
+    }
+
+    private fun checkFileAndWait(fileName: String, directory: String): Boolean {
+        val path = "$directory/$fileName"
+        if (!checkFile(path)) {
+            Thread.sleep(3000)
+        }
+        return checkFile(path)
+    }
+}
